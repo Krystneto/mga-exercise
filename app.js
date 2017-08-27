1a943b4b8542927ae46b566af2ccdd542b199cd1
Kristopher Parrillo <kkparrillo@gmail.com>
2017-08-27 15:30:58 -0700

    adds isRequired nodes

--- app.js
+++ app.js
@@ -4,6 +4,43 @@ const mobliePhoneInput = document.body.querySelector('#mobile_phone_number');
 const stateInput = document.body.querySelector("#state");
 const questionInput = document.querySelector("#question");
 
+const fullNameClass = document.body.querySelector('.full_name');
+const mobilePhoneClass = document.body.querySelector('.mobile_phone');
+const stateClass = document.body.querySelector('.state');
+const questionClass = document.body.querySelector('.question');
+
+function invalidNameField() {
+    let node = document.createElement('p');;
+    let fullNameRequiredText = 'Full Name is a required field.' 
+    node.className = 'isRequired'
+    node.innerText = fullNameRequiredText;
+    fullNameClass.appendChild(node)
+}
+
+function invalidMobilePhoneField() {
+    let node = document.createElement('p');;
+    let mobilePhoneRequiredText = 'Please enter a valid phone number.';
+    node.className = 'isRequired'
+    node.innerText = mobilePhoneRequiredText;
+    mobilePhoneClass.appendChild(node);
+}
+
+function invalidStateField() {
+    let node = document.createElement('p');;
+    let stateRequiredText = 'State is a required field.'    
+    node.className = 'isRequired'
+    node.innerText = stateRequiredText
+    stateClass.appendChild(node)
+}
+
+function invalidQuestionField() {
+    let node = document.createElement('p');;
+    let descriptionRequiredText = 'Description is a required field.'    
+    node.className = 'isRequired'
+    node.innerText = descriptionRequiredText
+    questionClass.appendChild(node);
+}
+
 function isValidFullName(value) {
 
 }
@@ -24,4 +61,16 @@ function widgetValidation() {
 
 }
 
-submitButton.addEventListener('click', widgetValidation);
+function something() {
+    invalidNameField()
+    invalidMobilePhoneField()
+    invalidStateField()
+    invalidQuestionField()
+}
+
+
+
+
+
+
+submitButton.addEventListener('click', something);
