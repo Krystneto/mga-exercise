aa16bcf558a51fed054c1850db5c660d0e3b32ec
Kristopher Parrillo <kkparrillo@gmail.com>
2017-08-27 13:17:25 -0700

    adds queryselectors for inputs

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('working');
+const submitButton = document.body.querySelector('#submit');
+const fullNameInput = document.body.querySelector('#full_name');
+const mobliePhoneInput = document.body.querySelector('#mobile_phone_number');
+const stateInput = document.body.querySelector("#state");
+const questionInput = document.querySelector("#question");
+
+
+
+
+
+
+
+
+
+
+
+submitButton.addEventListener('click', () => console.log("full_name value is", fullNameInput.value,
+    "mobilePhoneInput", mobile_phone_number.value)
+)
